592a25af02828777eb8ad2a6d96617a25d3a1555
sohyun1568 <sohyun9945@gmail.com>
2021-11-30 08:43:25 +0900

    2주차 예제

---
+++ sohyun/week2/js.js
@@ -0,0 +1,59 @@
+let obj = {'start': 1, 'end': 50};
+
+const arr = new Array(obj.end)
+.fill(obj.start)
+.map((val, idx) => val += idx)
+.filter((a) => a % 2 == 0);
+
+console.log(arr);
+
+obj.start = obj.start % 2 ? obj.start - 1 : obj.start;
+const iter = {
+    val: obj.start,
+    next() {
+        return {
+            value: this.val += 2,
+            done: this.val > obj.end,
+        }
+    },
+    [Symbol.iterator]() {
+        return this
+    }
+}
+
+console.log([...iter]);
+
+function* gen1(){
+    for(let i = obj.start; i <= obj.end; i ++){
+        i !== 0 && i % 2 === 0 ? yield i : 0;
+    }
+}
+
+console.log([...gen1()])
+
+function* gen2(){
+    yield* arr;
+}
+
+console.log([...gen2()])
+
+
+function* gen3(startNum){
+    while(startNum <= obj.end){
+        yield startNum
+        startNum += 2;
+    }
+} 
+
+console.log([...gen3(2)])
+
+const iterToGen = {
+    *[Symbol.iterator] () {
+        let num = obj.start + 2;
+        while(num <= obj.end){
+            yield num;
+            num += 2;
+        }
+    }
+}
+console.log([...iterToGen])
