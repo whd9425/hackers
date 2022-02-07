31474bbd5bb4812f8406c3efca33e8a60844cc2e
박종환 <whdghks94@gmail.com>
2022-02-07 21:56:26 +0900

    스터디

--- jonghwan/front/component/AppLayout.tsx
+++ jonghwan/front/component/AppLayout.tsx
@@ -3,7 +3,8 @@ import React, { useEffect , useState , useRef } from "react";
 import {Menu , Input , Row , Col} from 'antd';
 import UserProfile from '../component/UserProfile';
 import LoginForm from '../component/LoginForm';
-import styled from 'styled-components';
+import styled,{createGlobalStyle } from 'styled-components';
+import { useSelector } from 'react-redux';
 
 interface typeTest{
     children?: React.ReactNode  //React.ReactNode 리엑트node  ProTpes.node 와 마찬가지로 모든타입을 허용
@@ -17,38 +18,50 @@ const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `;
 
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+  
+  .ant-col:first-child {
+      padding-left: 0 !important;
+  }
+  
+  .ant-col:last-child {
+    padding-right: 0 !important;
+  }
+`;
+
+
+
 const AppLayout = ({children}: typeTest) =>{
-    const [isLoggedIn , setIsLoggedIn] = useState(false)
+    const { isLoggedIn } = useSelector(state => state.user);
     
     return (
         <div>
-            <Menu mode="horizontal">
-                <Menu.Item>
-                <Link href="/"><a>노드버드</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/profile"><a>프로필</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <SearchInput enterButton />  
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/signup"><a>회원가입</a></Link>
-                </Menu.Item>
-            </Menu>
-            <Row gutter={8}>
-                <Col xs={24} md={6}>
-                 {isLoggedIn ? <UserProfile  setIsLoggedIn={setIsLoggedIn}/> : <LoginForm  setIsLoggedIn={setIsLoggedIn}/>}     
-                </Col>
-                <Col xs={24} md={12}>
-                  {children}
-                </Col>
-                <Col xs={24} md={6}>
-                    <a href="https://www.naver.com" target="_blank" rel="noreferrer noopener">네이버가기</a>
-                </Col>
-            </Row>
-            
-        </div>
+      <Global />
+      <Menu mode="horizontal">
+        <Menu.Item key="home"><Link href="/"><a>노드버드</a></Link></Menu.Item>
+        <Menu.Item key="profile"><Link href="/profile"><a>프로필</a></Link></Menu.Item>
+        <Menu.Item key="mail">
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn
+            ? <UserProfile />
+            : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://www.zerocho.com" target="_blank" rel="noreferrer noopener">Made by ZeroCho</a>
+        </Col>
+      </Row>
+    </div>
     )
 
 }
--- jonghwan/front/component/LoginForm.tsx
+++ jonghwan/front/component/LoginForm.tsx
@@ -1,49 +1,41 @@
-import {Form , Input ,Button} from 'antd';
-import React, { useEffect , useState , useCallback , useMemo } from "react";
-import Link from 'next/Link';
-import styled from 'styled-components';
+import React, { useCallback } from 'react';
+import { Button, Form, Input } from 'antd';
+import Link from 'next/link';
+import { useDispatch } from 'react-redux';
+
 import useInput from '../hooks/useInput';
-import {Propss} from '../component/AppLayout'
+import { loginAction } from '../reducers/user';
 
+const LoginForm = () => {
+  const [id, onChangeId] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const dispatch = useDispatch();
 
-const ButtonWrapper = styled.div`
-  margin-top: 10px;
-`;
+  const onSubmitForm = useCallback(() => {
+    dispatch(loginAction({
+      id,
+      password,
+    }));
+  }, [id, password]);
 
-const FormWrapper =styled(Form)`padding:10px`;
+  return (
+    <Form onFinish={onSubmitForm} style={{ padding: '10px' }}>
+      <div>
+        <label htmlFor="user-id">아이디</label>
+        <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-password">비밀번호</label>
+        <br />
+        <Input name="user-password" value={password} onChange={onChangePassword} type="password" required />
+      </div>
+      <div style={{ marginTop: '10px' }}>
+        <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+        <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+      </div>
+    </Form>
+  );
+};
 
-const LoginForm = ({setIsLoggedIn}: Propss) =>{
-    const [id,onChangeId] = useInput('');
-    const [password, onChangePassword] = useInput('');
-
-   
-    const style = useMemo(() => ({marginTop:10}),[])
- 
-    const onSubmitForm = useCallback(() => {
-        console.log(id,password)
-        setIsLoggedIn(true)
-
-    }, [id,password])
-
-    return(
-        <FormWrapper onFinish={onSubmitForm}>
-            <div>
-                <label htmlFor="user-id">아이디</label>
-                <br/>
-                <Input name="user-id" value={id} onChange={onChangeId} required/>
-            </div>
-            <div>
-                <label htmlFor="user-password">비밀번호</label>
-                <br/>
-                <Input name="user_password" type="password" value={password} onChange={onChangePassword} required/>
-            </div>
-            <ButtonWrapper>
-                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
-                <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-            </ButtonWrapper>
-        </FormWrapper>
-    )
-
-}
-
-export default LoginForm
+export default LoginForm;
